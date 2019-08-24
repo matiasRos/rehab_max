162a06c456f4a1bcd60dc51eb9691cc45146d231
Matias <matias.ros@konecta.com.py>
2019-08-24 14:50:02 -0400

    Se agrega configuraciones de proxy

---
+++ proxy.conf.js
@@ -0,0 +1,17 @@
+const PROXY_CONFIG = [
+    {
+      context: [
+        "/stock-pwfe/",
+      ],
+      "target": "https://gy7228.myfoscam.org:8443",
+      "secure": false,
+      "logLevel": "debug",
+      "changeOrigin": true,
+      "pathRewrite": {
+        "^/": ""
+      }
+    }
+  ];
+  
+  module.exports = PROXY_CONFIG;
+  
---
+++ src/app/config/api.ts
@@ -0,0 +1,16 @@
+import {environment} from '../../environments/environment';
+
+
+const url = '/stock-pwfe/';
+
+
+
+
+
+const Servers = {
+  RehaMax: {
+    baseUrl: `${url}`
+  }
+};
+
+export { Servers };
