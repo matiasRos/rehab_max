2a866625ad8415a9b2ff7bea6a313f3f468ac96e
Matias <matias.ros@konecta.com.py>
2019-08-26 21:40:33 -0400

    Se corrige login y se agrega estilos

--- src/app/components/common/sidebar/sidebar.component.ts
+++ src/app/components/common/sidebar/sidebar.component.ts
@@ -14,8 +14,8 @@ export const ROUTES: RouteInfo[] = [
     { path: '/maps', title: 'Mapas',  icon:'ni-pin-3 text-orange', class: '' },
     { path: '/user-profile', title: 'Formulario',  icon:'ni-single-02 text-yellow', class: '' },
     { path: '/tables', title: 'Tablas',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
-    { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
+    { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' }
+    // { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
 ];
 
 @Component({
