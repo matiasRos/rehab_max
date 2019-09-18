c38189d2661b414ea73ad4a228e416c5ccc74a1f
Juan <juanra.vargas@gmail.com>
2019-09-18 15:18:01 -0400

    Ejercicio 9 POSTs y DELETES

--- src/app/services/registrar.service.ts
+++ src/app/services/registrar.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpResponse } from "@angular/common/http";
-import { Servers } from "../config/api";
-import { Observable } from "rxjs";
-import { catchError, map } from "rxjs/operators";
-import { handleError } from "../util/error-handler";
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
+import { Servers } from '../config/api';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { handleError } from '../util/error-handler';
 
 @Injectable({
   providedIn: 'root'
@@ -27,10 +27,51 @@ export class RegistrarService {
           usuario: 'ana'
         }
       })
-      .pipe(catchError(handleError("codigoMensaje", {})));
+      .pipe(catchError(handleError('codigoMensaje', {})));
   }
 
   listarServicios(): Observable<any> {
-    return this.http.get<any[]>(this.url).pipe(catchError(handleError("CodigoMensaje", {})));
+    return this.http.get<any[]>(this.url).pipe(catchError(handleError('CodigoMensaje', {})));
+  }
+
+  listarServicioPorIdServicioCabecera(idServicio): Observable<any> {
+    return this.http.get<any[]>(this.url + '/' + idServicio).pipe(catchError(handleError('CodigoMensaje', {})));
+  }
+
+  listarServicioPorIdServicioDetalle(idServicio): Observable<any> {
+    return this.http.get<any[]>(this.url + '/' + idServicio + '/detalle').pipe(catchError(handleError('CodigoMensaje', {})));
+  }
+  obtenerServiciosRegistradosPorFisioterapeuta(idServicio): Observable<any> {
+    return this.http.get<any[]>(this.url + 'ejemplo={"idFichaClinica":{"idCliente":{"idPersona":' + idServicio + '}}}');
+  }
+
+  listarPresentacionProducto(): Observable<any> {
+    return this.http.get<any[]>(Servers.RehabMax.baseUrl + '/presentacionProducto');
+  }
+  listarObtenerServiciosPorPaciente(idPaciente): Observable<any> {
+    return this.http.get<any[]>(this.url + '/stock-pwfe /servicio?ejemplo={"idFichaClinica":{"idCliente":{"idPersona":'
+      + idPaciente + '}}}');
+  }
+  obtenerServiciosRealizadosPorFecha(fechaDesde, fechaHasta): Observable<any> {
+    return this.http.get<any[]>(this.url + '?ejemplo={"fechaDesdeCadena":' + fechaDesde + ',"fechaHastaCadena":' + fechaHasta);
+  }
+
+  deleteUnServicio(idServicio, idDetalle): Observable<any> {
+    return this.http.delete(this.url + '/' + idServicio + '/detalle/' + idDetalle);
+  }
+
+  agregarDetallesAServicio(idServicio, idPresentacionProducto, cantidad): Observable<any> {
+    var body = {
+      cantidad: cantidad,
+      idPresentacionProducto: {
+        idPresentacionProducto: idPresentacionProducto
+      },
+      idServicio: { idServicio: idServicio }
+    };
+    return this.http.post(this.url, body, {
+      headers: {
+        usuario: 'ana'
+      }
+    });
   }
 }
